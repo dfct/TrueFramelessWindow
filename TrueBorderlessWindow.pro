7264ac73c3bbfbd6a6f790860e44c0586b58d412
Layer Shape <admin@layershape.net>
2016-08-22 14:51:40 -0400

    Code commit

---
+++ TrueBorderlessWindow.pro
@@ -0,0 +1,46 @@
+#-------------------------------------------------
+#
+# TrueBorderlessWindow.pro
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = TrueBorderlessWindow
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+    widget.cpp
+
+HEADERS  += widget.h
+
+
+win32 {
+    # Only include / compile these files on Windows
+    SOURCES += \
+        qwinwidget.cpp \
+        winnativewindow.cpp
+
+    HEADERS += \
+        qwinwidget.h \
+        winnativewindow.h
+
+    LIBS += dwmapi.lib
+}
+
+mac {
+    # Only include / compile these files on OS X
+    OBJECTIVE_SOURCES += \
+        OSXHideTitleBar.mm
+    HEADERS  +=\
+        OSXHideTitleBar.h
+
+    # Additionally include Cocoa for OS X code
+
+    LIBS += -framework Foundation -framework Cocoa
+    INCLUDEPATH += /System/Library/Frameworks/Foundation.framework/Versions/C/Headers \
+
+}
---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <QApplication>
+
+#ifdef _WIN32
+#include "QWinWidget.h"
+#else
+#include "Widget.h"
+#endif
+
+#ifdef __APPLE__
+#include "OSXHideTitleBar.h"
+#endif
+
+int main(int argc, char *argv[])
+{    
+    //This has the app draw at HiDPI scaling on HiDPI displays, usually two pixels for every one logical pixel
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    //This has QPixmap images use the @2x images when available
+    //See this bug for more details on how to get this right: https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+
+
+    QApplication app(argc, argv);
+
+
+
+    //A common feature is to save your app's geometry on close such that you can draw in the same place on relaunch
+    //Thus this project supports specifying the X/Y/Width/Height in a cross-platform manner
+    int windowXPos, windowYPos, windowWidth, windowHeight;
+    windowXPos = 100;
+    windowYPos = 100;
+    windowWidth = 1024;
+    windowHeight = 768;
+
+
+#ifdef _WIN32
+
+    //On Windows, the widget needs to be encapsulated in a native window for frameless rendering
+    //In this case, QWinWidget #includes "Widget.h", creates it, and adds it to a layout
+	QWinWidget qWinWidget(windowXPos, windowYPos, windowWidth, windowHeight);
+    qWinWidget.show();
+
+    return app.exec();
+
+#endif
+
+#ifdef __APPLE__
+
+    //On OS X, the widget can handle everything itself so just create Widget as normal
+    Widget w;
+	w.setGeometry(windowXPos, windowYPos, windowWidth, windowHeight);
+
+    //Then, hide the OS X title bar
+    OSXHideTitleBar::HideTitleBar(w.winId());
+
+    w.show();
+    return app.exec();
+
+#else
+
+    //Non-Windows Non-Apple platform
+    //Just create Widget as normal
+    Widget w;
+    w.setGeometry(windowXPos, windowYPos, windowWidth, windowHeight);
+
+    w.show();
+    return app.exec();
+
+#endif
+}
